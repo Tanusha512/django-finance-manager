589e18c9d78c4a625087167c3c9ca54a29353d17
Tanusha512 <tanusha051204@yandex.ru>
2026-01-27 00:54:53 +0300

    Создана русская панель Dashboard с кошельком, тарнзакциями и целями

--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Wallet, Transaction, Goal
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 def dashboard(request):
     return HttpResponse("Финансовый менеджер работает!")
+
+@login_required
+def dashboard(request):
+    user = request.user
+    wallets = Wallet.objects.filter(user=user)
+    transactions = Transaction.objects.filter(wallet__user=user).order_by('-date')[:10]
+    goals = Goal.objects.filter(user=user)
+
+    context = {
+        'wallets': wallets,
+        'transactions': transactions,
+        'goals': goals,
+    }
+    return render(request, 'finance/dashboard.html', context)
